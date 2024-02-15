aa104eb1a49d94feed5d7b0105a19793e0da8456
Matt Garcia <prebccentral@gmail.com>
2024-02-15 13:48:30 -0700

    Added working sprites, basic game HUD
    Co-authored-by: Florenzo Bauer <FlorenzoBauer@users.noreply.github.com>

---
+++ src/components/BattleMenu/BattleMenu.jsx
@@ -0,0 +1,13 @@
+import './BattleMenu.css'
+
+const BattleMenu = ({onAttack, onAttack2, onAttack3, onHeal}) => {
+    return (
+        <div className="battle-menu">
+        <button onClick={onAttack} className="battle-menu__button">Attack1</button>
+        <button onClick={onAttack2} className="battle-menu__button">Attack2</button>
+        <button onClick={onAttack3} className="battle-menu__button">Attack3</button>
+        <button onClick={onHeal} className="battle-menu__button">Heal</button>
+        </div>
+    )
+    }
+    export default BattleMenu;
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,20 +1,30 @@
 import './Game.css'
 import PlayerSummary from '../PlayerSummary/PlayerSummary'
 import { enemyStats, playerStats } from '../../shared/Characters'
+import BattleMenu from '../BattleMenu/BattleMenu'
+
 const Game = () => {
     const [opponentHealth, setOpponentHealth] = [enemyStats.maxHealth]
     const [playerHealth, setPlayerHealth] = [playerStats.maxHealth]
 
     return (
-        <div>
-
+        <game>
             <div>
                 <PlayerSummary 
-                health={40} 
+                health={playerHealth} 
                 name={playerStats.name} 
                 level={playerStats.level} 
                 maxHealth={playerStats.maxHealth} 
                 className='playerSummary' main={false} />
+            </div>
+            <div className='characters-vs-sign' >
+                {playerStats.name} vs {enemyStats.name}
+            </div>
+            <div className='player-characters'>
+                <img className='character-imgs' src={playerStats.img} alt={playerStats.name} />
+            </div>
+            <div className='enemy-characters'>
+                <img className='character-imgs' src={enemyStats.img} alt={enemyStats.name} />
             </div>
             <div>
                 <PlayerSummary 
@@ -24,8 +34,16 @@ const Game = () => {
                 maxHealth={enemyStats.maxHealth} 
                 className='AiSummary' main={true}/>
             </div>
-
-        </div>
+            <div className='hud'>
+                <BattleMenu className='battle-menu'
+                onAttack={() => {console.log('Attack')}}
+                onAttack2={() => {console.log('Attack2')}}
+                onAttack3={() => {console.log('Attack3')}}
+                onHeal={() => {console.log('Heal')}}
+                
+                />
+            </div>
+        </game>
     )
 }
 
