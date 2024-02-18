581cb12246430c28f8f01756d100dbc38cacc9e0
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-02-17 19:30:09 -0600

    Changes Hooks name to remove error with function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
