c0825f65c69b826393356b201cdbdaab9df2a969
Matt Garcia <MattpGarcia13@users.noreply.github.com>
2024-02-20 21:33:30 -0700

    Added functionality to all characters

--- src/shared/Characters.js
+++ src/shared/Characters.js
@@ -1,5 +1,13 @@
 import angryCloudSprite from '../assets/angrycloudsprite.png';
 import squirreldemonsprite from '../assets/squirreldemonsprite.png';
+import happyGhostSprite from '../assets/happyghostsprite.png';
+import detectiveFrogSprite from '../assets/detectivefrogsprite.png';
+import grassEagleSprite from '../assets/grasseaglesprite.png';
+import liquidDogSprite from '../assets/liquiddogsprite.png';
+import timeMonsterSprite from '../assets/timemonstersprite.png';
+import fairyBugSprite from '../assets/fairybugsprite.png';
+import electricSpiderSprite from '../assets/electricspidersprite.png';
+import flameGiraffeSprite from '../assets/flamegiraffesprite.png';
 
 export const characters = {
     angryCloudSprite: {
@@ -20,6 +28,110 @@ export const characters = {
         maxHealth: 150,
         name: 'Squirrel Demon',
         img: squirreldemonsprite,
+        attacks: [
+            { name: 'attack1', damage: 20 },
+            { name: 'attack2', damage: 30 },
+            { name: 'attack3', damage: 480 },
+            { name: 'heal', damage: -40 }
+        ],
+        defense: 20
+    },
+    happyGhostSprite: {
+        level: 1,
+        maxHealth: 150,
+        name: 'Happy Ghost',
+        img: happyGhostSprite,
+        attacks: [
+            { name: 'attack1', damage: 20 },
+            { name: 'attack2', damage: 30 },
+            { name: 'attack3', damage: 480 },
+            { name: 'heal', damage: -40 }
+        ],
+        defense: 20
+    },
+    detectiveFrogSprite: {
+        level: 1,
+        maxHealth: 150,
+        name: 'Detective Frog',
+        img: detectiveFrogSprite,
+        attacks: [
+            { name: 'attack1', damage: 20 },
+            { name: 'attack2', damage: 30 },
+            { name: 'attack3', damage: 480 },
+            { name: 'heal', damage: -40 }
+        ],
+        defense: 20
+    },
+    grassEagleSprite: {
+        level: 1,
+        maxHealth: 150,
+        name: 'Earth Eagle',
+        img: grassEagleSprite,
+        attacks: [
+            { name: 'attack1', damage: 20 },
+            { name: 'attack2', damage: 30 },
+            { name: 'attack3', damage: 480 },
+            { name: 'heal', damage: -40 }
+        ],
+        defense: 20
+    },
+    liquidDogSprite: {
+        level: 1,
+        maxHealth: 150,
+        name: 'Liquid Cerberus',
+        img: liquidDogSprite,
+        attacks: [
+            { name: 'attack1', damage: 20 },
+            { name: 'attack2', damage: 30 },
+            { name: 'attack3', damage: 480 },
+            { name: 'heal', damage: -40 }
+        ],
+        defense: 20
+    },
+    timeMonsterSprite: {
+        level: 1,
+        maxHealth: 150,
+        name: 'Time Monster',
+        img: timeMonsterSprite,
+        attacks: [
+            { name: 'attack1', damage: 20 },
+            { name: 'attack2', damage: 30 },
+            { name: 'attack3', damage: 480 },
+            { name: 'heal', damage: -40 }
+        ],
+        defense: 20
+    },
+    fairyBugSprite: {
+        level: 1,
+        maxHealth: 150,
+        name: 'Mystical Beatle',
+        img: fairyBugSprite,
+        attacks: [
+            { name: 'attack1', damage: 20 },
+            { name: 'attack2', damage: 30 },
+            { name: 'attack3', damage: 480 },
+            { name: 'heal', damage: -40 }
+        ],
+        defense: 20
+    },
+    electricSpiderSprite: {
+        level: 1,
+        maxHealth: 150,
+        name: 'Electric Spider',
+        img: electricSpiderSprite,
+        attacks: [
+            { name: 'attack1', damage: 20 },
+            { name: 'attack2', damage: 30 },
+            { name: 'attack3', damage: 480 },
+            { name: 'heal', damage: -40 }
+        ],
+        defense: 20
+    },
+    flameGiraffeSprite: {
+        level: 1,
+        maxHealth: 150,
+        name: 'Flaming Giraffe',
+        img: flameGiraffeSprite,
         attacks: [
             { name: 'attack1', damage: 20 },
             { name: 'attack2', damage: 30 },
