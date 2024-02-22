9b5151aac9e5c7704dbc170bc848f720041677fc
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-02-22 03:31:22 -0600

    Clean up Game.jsx file

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,97 +1,99 @@
-import './Game.css'
-import PlayerSummary from '../PlayerSummary/PlayerSummary'
-import BattleMenu from '../BattleMenu/BattleMenu'
-import Announcer from '../Announcer/Announcer'
-import { useEffect, useState } from 'react'
-import { useBattleSequence } from '../../hooks/useBattleSequence'
-import { useAIOpponent } from '../../hooks/useAIOpponent'
-import { wait } from '../../shared/Helpers'
-import { EndGame } from '../EndGame/EndGame'
-import { Navigate, useNavigate } from 'react-router-dom'
-import { useLocation } from 'react-router-dom'
+import './Game.css';
+import PlayerSummary from '../PlayerSummary/PlayerSummary';
+import BattleMenu from '../BattleMenu/BattleMenu';
+import Announcer from '../Announcer/Announcer';
+import { useEffect, useState } from 'react';
+import { useBattleSequence } from '../../hooks/useBattleSequence';
+import { useAIOpponent } from '../../hooks/useAIOpponent';
+import { wait } from '../../shared/Helpers';
+import { Navigate, useNavigate, useLocation } from 'react-router-dom';
 
-const Game = ({ winner, setWinner}) => {
+const Game = ({ winner, setWinner }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const playerStats = location.state.playerStats;
+  const enemyStats = location.state.enemyStats;
 
-    const location = useLocation();
-    const navigate = useNavigate();
-    const playerStats = location.state.playerStats;
-    const enemyStats = location.state.enemyStats;
-    if(enemyStats.level > 1){
-        const upgrades = enemyStats.level * 20
-        enemyStats.maxHealth += upgrades
-        enemyStats.defense  += upgrades
+  useEffect(() => {
+    if (enemyStats.level > 1) {
+      const upgrades = enemyStats.level * 20;
+      enemyStats.maxHealth += upgrades;
+      enemyStats.defense += upgrades;
     }
-    
-    const [sequence, setSequence] =useState({});
-    const {
-        turn,
-        inSequence,
-        opponentHealth,
-        playerHealth,
-        announcerMessage 
-    } = useBattleSequence({playerStats, enemyStats, sequence})
+  }, [enemyStats.level]);
 
-    const aiChoice = useAIOpponent(turn)
+  const [sequence, setSequence] = useState({});
+  const {
+    turn,
+    inSequence,
+    opponentHealth,
+    playerHealth,
+    announcerMessage
+  } = useBattleSequence({ playerStats, enemyStats, sequence });
 
-    useEffect(() => {
-        if(aiChoice && turn === 1 && !inSequence) {
-          setSequence({ turn, mode: aiChoice })
-        }
-    }, [turn, aiChoice, inSequence])
-    
-    useEffect(() => {
-        if (playerHealth === 0 || opponentHealth === 0) {
-          (async () => {
-            await wait(1000);
-            setWinner({
-              winner: playerHealth > 0 ? playerStats : null
-            });
-            navigate('/endgame')
-          })();
-        }
-      }, [playerHealth, opponentHealth]);
+  const aiChoice = useAIOpponent(turn);
 
-    return (
-        <div className='game'>
-            <div className='player2summary'>
-                <PlayerSummary
-                health={opponentHealth}
-                name={enemyStats.name}
-                level={enemyStats.level}
-                maxHealth={enemyStats.maxHealth}
-                className='AiSummary' main={true}/>
-            </div>
-            {/* <div className='characters-vs-sign' >
-                {playerStats.name} vs {enemyStats.name}
-            </div> */}
-            <div className='enemy-characters'>
-                <img className='enem-imgs' src={enemyStats.img} alt={enemyStats.name} />
-            </div>
-            <div className='player-characters'>
-                <img className='character-imgs' src={playerStats.img} alt={playerStats.name} />
-            </div>
-            <div className='player1summary'>
-                <PlayerSummary
-                health={playerHealth} 
-                name={playerStats.name}
-                level={playerStats.level}
-                maxHealth={playerStats.maxHealth}
-                className='playerSummary' main={false} />
-            </div>
-            <div className='hud'>
-                <Announcer message={
-                    announcerMessage || `What will ${playerStats.name} do?`
-                }/>
+  useEffect(() => {
+    if (aiChoice && turn === 1 && !inSequence) {
+      setSequence({ turn, mode: aiChoice });
+    }
+  }, [turn, aiChoice, inSequence]);
 
-                <BattleMenu className='battle-menu'
-                onAttack1={() => setSequence({ turn, mode: playerStats.attacks[0].name })}
-                onAttack2={() => setSequence({ turn, mode: playerStats.attacks[1].name})}
-                onAttack3={() => setSequence({ turn, mode: playerStats.attacks[2].name})}
-                onHeal={() => setSequence({ turn, mode: playerStats.attacks[3].name })}
-                />
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    if (playerHealth === 0 || opponentHealth === 0) {
+      (async () => {
+        await wait(1000);
+        setWinner({
+          winner: playerHealth > 0 ? playerStats : null
+        });
+        navigate('/endgame');
+      })();
+    }
+  }, [playerHealth, opponentHealth]);
+
+  return (
+    <div className='game'>
+      <div className='player2summary'>
+        <PlayerSummary
+          health={opponentHealth}
+          name={enemyStats.name}
+          level={enemyStats.level}
+          maxHealth={enemyStats.maxHealth}
+          className='AiSummary'
+          main={true}
+        />
+      </div>
+      <div className='enemy-characters'>
+        <img className='enem-imgs' src={enemyStats.img} alt={enemyStats.name} />
+      </div>
+      <div className='player-characters'>
+        <img className='character-imgs' src={playerStats.img} alt={playerStats.name} />
+      </div>
+      <div className='player1summary'>
+        <PlayerSummary
+          health={playerHealth}
+          name={playerStats.name}
+          level={playerStats.level}
+          maxHealth={playerStats.maxHealth}
+          className='playerSummary'
+          main={false}
+        />
+      </div>
+      <div className='hud'>
+        <Announcer
+          message={announcerMessage || `What will ${playerStats.name} do?`}
+        />
+
+        <BattleMenu
+          className='battle-menu'
+          onAttack1={() => setSequence({ turn, mode: playerStats.attacks[0].name })}
+          onAttack2={() => setSequence({ turn, mode: playerStats.attacks[1].name })}
+          onAttack3={() => setSequence({ turn, mode: playerStats.attacks[2].name })}
+          onHeal={() => setSequence({ turn, mode: playerStats.attacks[3].name })}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Game;
